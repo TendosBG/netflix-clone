7b7773cbda6ee6300f2249e803f9ea9ed6e50c74
Maxime Delhaye <maxime.delhaye28@gmail.com>
2024-07-21 18:15:28 +0200

    Banner implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import './styles/App.css'
 import Row from './components/Row'
 import requests from './scripts/request'
+import Banner from "./components/Banner.tsx";
 
 function App() {
 
     return (
         <>
-            <h1>My Netflix Clone.</h1>
+            <Banner />
+
             <Row title={"Seulement sur Netflix"} fetchURL={requests.fetchNetflixOriginals} isLargeRow={true}/>
             <Row title={"Tendance en ce moment"} fetchURL={requests.fetchTrending}/>
             <Row title={"Films salués par la critique"} fetchURL={requests.fetchTopRated}/>
             <Row title={"Films d'action"} fetchURL={requests.fetchActionMovies}/>
             <Row title={"Comédie"} fetchURL={requests.fetchComedyMovies}/>
-            <Row title={"Films d'horreur"} fetchURL={requests.fetchHorrorMovies}/>
+            <Row title={"Horreur"} fetchURL={requests.fetchHorrorMovies}/>
             <Row title={"Romance"} fetchURL={requests.fetchRomanceMovies}/>
             <Row title={"Documentaires"} fetchURL={requests.fetchDocumentaries}/>
         </>
